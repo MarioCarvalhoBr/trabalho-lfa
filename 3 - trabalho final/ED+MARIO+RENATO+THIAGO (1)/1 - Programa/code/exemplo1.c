fd9cffe897880fe4d9fcc0f8fcb452c70f799d39
Mario Carvalho <mariodearaujocarvalho@gmail.com>
2018-06-12 20:39:40 -0400

    Trabalho final

---
+++ 3 - trabalho final/ED+MARIO+RENATO+THIAGO (1)/1 - Programa/code/exemplo1.c
@@ -0,0 +1,65 @@
+int a;
+int a1 , b;
+int y = 5;
+int a2 = 5, b2 = 5, d = 6, c = 8, p = 10;
+int a3 , b3 = 5, c3;
+int a4 = 5, b4;
+
+int f(){
+
+}
+int a = 5;
+int a(){
+
+}
+
+void main()
+{
+  int a;
+  int a1 , b;
+  int y = 5;
+  int a2 = 5, b2 = 5, d = 6, c = 8, p = 10;
+  int a3 , b3 = 5, c3;
+  int a4 = 5, b4;
+
+  a = 10;
+  b = 3;
+  while (a)
+  {
+    c = ( a - b) * 7;
+    c = (a + b) * 2;
+    c = (a + b) / 2;
+    a = a + 1 + (b + (c + 1) * d);
+    b = b * b;
+  }
+  a = 10;
+  while (b)
+    a = 10;
+  a = 10;
+
+  a++;
+  b--;
+
+  a = (a++) + 5;
+  a = ++a;
+  a = --a;
+  b = --a + 7;
+
+
+  a += 1;
+  a += a + 1;
+  a += (a + b);
+  
+  a -= 1;
+  a -= a + 1;
+  a -= (a + b);
+
+  a *= 1;
+  a *= a + 1;
+  a *= (a + b);
+
+  a /= 1;
+  a /= a + 1;
+  a /= (a + b);
+
+}
